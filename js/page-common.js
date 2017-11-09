7f88f6f2b6918042f48d2ca0e402d397921c57db
coreyzeng <coreyzeng@qeeka.com>
2017-11-09 16:29:30 +0800

    UI

--- js/page-common.js
+++ js/page-common.js
@@ -1,3 +1,17 @@
+var Env = {
+    "qzsDomain": "qzs.qq.com",
+    "zcDomain": "c.qq.com",
+    "cDomain": "c.qq.com",
+    "mDomain": "m.c.qq.com",
+    "adminDomain": "zcoa.3gqq.com",
+    "qtengDomain": "qteng.qq.com",
+    "statDomain": "stat.c.qq.com",
+    "vcDomain": "vc.open.qq.com",
+    "tgpcDomain": "tgpc.qq.com",
+    "title": "\u6b63\u5f0f\u73af\u5883",
+    "id": "production",
+    "cgiDomain": "c.qq.com"
+};
 $(function () {
     common.init();
 })
